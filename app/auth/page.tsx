fbea8b0d6ec74f14f1570cd0fd3cd0541f906233
aida <aidasaeedi1998@gmail.com>
2025-04-05 23:45:30 +0330

    auth page

---
+++ app/auth/page.tsx
@@ -0,0 +1,12 @@
+import AuthForm from "@/components/AuthForm";
+
+export default function AuthPage() {
+  return (
+    <main className="max-w-sm mx-auto mt-20">
+      <h1 className="text-2xl font-bold mb-4 text-center text-violet-950">
+        Authentication
+      </h1>
+      <AuthForm />
+    </main>
+  );
+}
