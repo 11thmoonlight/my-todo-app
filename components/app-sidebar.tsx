7836ce7945322921fb757bfeca2b154abe54eb8b
aida <aidasaeedi1998@gmail.com>
2025-04-06 23:18:49 +0330

    fill sidebar items

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,110 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuBadge,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarSeparator,
+} from "@/components/ui/sidebar";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "./ui/collapsible";
+import { ChevronDown } from "lucide-react";
+import { LuSettings } from "react-icons/lu";
+import { PiSignOutBold } from "react-icons/pi";
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader>
+        <span className="font-semibold text-lg">Menu</span>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarMenu className="overflow-hidden">
+          <Collapsible defaultOpen className="group/collapsible">
+            <SidebarGroup>
+              <SidebarGroupLabel asChild>
+                <CollapsibleTrigger className="">
+                  TASKS
+                  <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+                </CollapsibleTrigger>
+              </SidebarGroupLabel>
+              <CollapsibleContent>
+                <SidebarMenuItem>
+                  <SidebarMenuButton>Upcoming</SidebarMenuButton>
+                  <SidebarMenuBadge>4</SidebarMenuBadge>
+                </SidebarMenuItem>
+                <SidebarMenuItem>
+                  <SidebarMenuButton>Today</SidebarMenuButton>
+                  <SidebarMenuBadge>4</SidebarMenuBadge>
+                </SidebarMenuItem>
+                <SidebarMenuItem>
+                  <SidebarMenuButton>Calender</SidebarMenuButton>
+                </SidebarMenuItem>
+                <SidebarMenuItem>
+                  <SidebarMenuButton>Sticky Wall</SidebarMenuButton>
+                </SidebarMenuItem>
+              </CollapsibleContent>
+            </SidebarGroup>
+          </Collapsible>
+
+          <SidebarSeparator />
+
+          <Collapsible defaultOpen className="group/collapsible">
+            <SidebarGroup>
+              <SidebarGroupLabel asChild>
+                <CollapsibleTrigger>
+                  LISTS
+                  <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+                </CollapsibleTrigger>
+              </SidebarGroupLabel>
+              <CollapsibleContent>
+                <SidebarMenuItem>
+                  <SidebarMenuButton>Personal</SidebarMenuButton>
+                  <SidebarMenuBadge>4</SidebarMenuBadge>
+                </SidebarMenuItem>
+                <SidebarMenuItem>
+                  <SidebarMenuButton>Work</SidebarMenuButton>
+                  <SidebarMenuBadge>4</SidebarMenuBadge>
+                </SidebarMenuItem>
+                <SidebarMenuItem>
+                  <SidebarMenuButton>Add New List</SidebarMenuButton>
+                </SidebarMenuItem>
+              </CollapsibleContent>
+            </SidebarGroup>
+          </Collapsible>
+
+          <SidebarSeparator />
+
+          <Collapsible defaultOpen className="group/collapsible">
+            <SidebarGroup>
+              <SidebarGroupLabel asChild>
+                <CollapsibleTrigger>
+                  TAGS
+                  <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+                </CollapsibleTrigger>
+              </SidebarGroupLabel>
+              <CollapsibleContent>
+                <SidebarGroupContent />
+              </CollapsibleContent>
+            </SidebarGroup>
+          </Collapsible>
+
+          <SidebarGroup />
+        </SidebarMenu>
+      </SidebarContent>
+      <SidebarFooter>
+        <p>Setteings</p>
+        <p>Sign Out</p>
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
