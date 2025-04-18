89239e2ef7b545861a5f6e61d2ddd43237cabb6f
aida <aidasaeedi1998@gmail.com>
2025-04-18 23:03:42 +0330

    add list and tag form

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -3,7 +3,6 @@ import {
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
-  SidebarGroupContent,
   SidebarGroupLabel,
   SidebarHeader,
   SidebarMenu,
@@ -26,100 +25,11 @@ import { FaListCheck } from "react-icons/fa6";
 import { LuCalendarDays } from "react-icons/lu";
 import { LuStickyNote } from "react-icons/lu";
 import { MdOutlineCheckBoxOutlineBlank } from "react-icons/md";
-import { IoMdAdd } from "react-icons/io";
-import {
-  Dialog,
-  DialogContent,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "./ui/dialog";
-import { Button } from "./ui/button";
-import { Label } from "./ui/label";
-import { Input } from "./ui/input";
+
+import NewListForm from "./NewListForm";
+import NewTagForm from "./NewTagForm";
 
 export function AppSidebar() {
-  const colors = [
-    {
-      value: "yellow",
-      bgClass: "bg-yellow-500",
-    },
-    {
-      value: "red",
-      bgClass: "bg-red-500",
-    },
-    {
-      value: "orange",
-      bgClass: "bg-orange-500",
-    },
-    {
-      value: "amber",
-      bgClass: "bg-amber-500",
-    },
-    {
-      value: "lime",
-      bgClass: "bg-lime-500",
-    },
-    {
-      value: "green",
-      bgClass: "bg-green-500",
-    },
-    {
-      value: "emerald",
-      bgClass: "bg-emerald-500",
-    },
-    {
-      value: "teal",
-      bgClass: "bg-teal-500",
-    },
-    {
-      value: "cyan",
-      bgClass: "bg-cyan-500",
-    },
-    {
-      value: "sky",
-      bgClass: "bg-sky-500",
-    },
-    {
-      value: "indigo",
-      bgClass: "bg-indigo-500",
-    },
-    {
-      value: "violet",
-      bgClass: "bg-violet-500",
-    },
-    {
-      value: "purple",
-      bgClass: "bg-purple-500",
-    },
-    {
-      Value: "fuchsia",
-      bgClass: "bg-fuchsia-500",
-    },
-    {
-      Value: "pink",
-      bgClass: "bg-pink-500",
-    },
-    {
-      Value: "rose",
-      bgClass: "bg-rose-500",
-    },
-    {
-      Value: "slate",
-      bgClass: "bg-slate-500",
-    },
-    {
-      Value: "zinc",
-      bgClass: "bg-zinc-500",
-    },
-    {
-      Value: "stone",
-      bgClass: "bg-stone-500",
-    },
-    { value: "neutral", bgClass: "bg-neutral-500" },
-  ];
-
   return (
     <Sidebar>
       <SidebarHeader className="text-violet-900">
@@ -205,57 +115,7 @@ export function AppSidebar() {
                 </SidebarMenuItem>
                 <SidebarMenuItem>
                   <SidebarMenuButton className="cursor-pointer">
-                    <Dialog>
-                      <DialogTrigger asChild>
-                        <div className="flex gap-2">
-                          <IoMdAdd size={20} className="text-violet-500" />
-                          Add New List
-                        </div>
-                      </DialogTrigger>
-                      <DialogContent className="sm:max-w-[425px] text-violet-900">
-                        <DialogHeader>
-                          <DialogTitle className="text-center font-bold text-xl mb-3">
-                            Add New List
-                          </DialogTitle>
-                        </DialogHeader>
-                        <div className="grid gap-4 py-4">
-                          <Input
-                            id="title"
-                            placeholder="List Title"
-                            className="col-span-3"
-                          />
-                        </div>
-
-                        <div className="flex flex-wrap gap-3 rtl:space-x-reverse items-center justify-center">
-                          {colors.map((color) => (
-                            <Label
-                              key={color.value}
-                              className="cursor-pointer hover:bg-yellow-100 hover:scale-105 rounded-lg active:scale-95 transition-all duration-300 mb-2"
-                            >
-                              <Input
-                                type="radio"
-                                name="priority"
-                                value={color.value}
-                                className="hidden peer"
-                              />
-                              <div
-                                className={`flex gap-1 items-end justify-center p-3 rounded-lg transition
-                  ${color.bgClass} peer-checked:bg-gray-200`}
-                              ></div>
-                            </Label>
-                          ))}
-                        </div>
-
-                        <DialogFooter>
-                          <Button
-                            type="submit"
-                            className="w-full mt-6 bg-violet-300 text-violet-950 hover:bg-violet-200 active:scale-80 transition-transform duration-300"
-                          >
-                            Add List
-                          </Button>
-                        </DialogFooter>
-                      </DialogContent>
-                    </Dialog>
+                    <NewListForm />
                   </SidebarMenuButton>
                 </SidebarMenuItem>
               </CollapsibleContent>
@@ -280,57 +140,7 @@ export function AppSidebar() {
                   Tag 2
                 </div>
                 <SidebarMenuButton className="cursor-pointer">
-                  <Dialog>
-                    <DialogTrigger asChild className="w-full">
-                      <div className="flex gap-1 bg-gray-200 p-2 rounded-md text-xs">
-                        <IoMdAdd size={16} className="text-violet-500" />
-                        <span className="font-semibold">Add New Tag</span>
-                      </div>
-                    </DialogTrigger>
-                    <DialogContent className="sm:max-w-[425px] text-violet-900">
-                      <DialogHeader>
-                        <DialogTitle className="text-center font-bold text-xl mb-3">
-                          Add New Tag
-                        </DialogTitle>
-                      </DialogHeader>
-                      <div className="grid gap-4 py-4">
-                        <Input
-                          id="title"
-                          placeholder="Tag Title"
-                          className="col-span-3"
-                        />
-                      </div>
-
-                      <div className="flex flex-wrap gap-3 rtl:space-x-reverse items-center justify-center">
-                        {colors.map((color) => (
-                          <Label
-                            key={color.value}
-                            className="cursor-pointer hover:bg-yellow-100 hover:scale-105 rounded-lg active:scale-95 transition-all duration-300 mb-2"
-                          >
-                            <Input
-                              type="radio"
-                              name="priority"
-                              value={color.value}
-                              className="hidden peer"
-                            />
-                            <div
-                              className={`flex gap-1 items-end justify-center p-3 rounded-lg transition
-                  ${color.bgClass} peer-checked:bg-gray-200`}
-                            ></div>
-                          </Label>
-                        ))}
-                      </div>
-
-                      <DialogFooter>
-                        <Button
-                          type="submit"
-                          className="w-full mt-6 bg-violet-300 text-violet-950 hover:bg-violet-200 active:scale-80 transition-transform duration-300"
-                        >
-                          Add List
-                        </Button>
-                      </DialogFooter>
-                    </DialogContent>
-                  </Dialog>
+                  <NewTagForm />
                 </SidebarMenuButton>
               </CollapsibleContent>
             </SidebarGroup>
